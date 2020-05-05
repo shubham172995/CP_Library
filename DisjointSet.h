b7e10b11c6c0434c38a9b860983b2bf8d2003cfb
shubham <shubham.wheofficial@gmail.com>
2020-05-06 00:21:56 +0530

    Add exisfing files

---
+++ DisjointSet.h
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define lloop(i, a, b) for(ll i=a;i<(ll)b; i++)
+#define loop(i, a, b) for(int i=a;i<b;i++)
+#define pool(i, a, b) for(int i=a;i>b;i--)
+#define pooll(i, a, b) for(ll i=a;i>b;i--)
+#define pb push_back
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef pair<ll, ll> llll;
+typedef vector<llll> vll;
+typedef vetor<pair<int, ii> > EdgeList; //Edgelist representation of graphs to store an edge and weights for both directions, i.e., in an undirected graph.x
+
+
+//								STARTS HERE	
+
+vi p, rank;
+
+CreateSet(int n){
+	rank.resize(n);
+	p.resize(n);
+	loop(i, 0, n){
+		p[i]=i;
+	}
+}
+
+int FindSet(int i){
+	return p[i]==i?i:(p[i]=FindSet(p[i]));
+}
+
+bool isSameSet(int x, int y){
+	return FindSet(x)==FindSet(y);
+}
+
+void unionSet(int x, int y){
+	if(!isSameSet(x, y)){
+		int px=FindSet(x), py=FindSet(y);	//Path Compression.
+		if(rank[px]>rank[py])
+			p[y]=x;
+		else{
+			p[x]=y;
+			rank[y]=rank[x]==rank[y]?rank[y]+1:rank[y];
+		}
+	}
+}
+
+int numDisjointSets(){
+	int num=0;
+	loop(i, 0, p.size()){
+		if(p[i]==i)
+			++num;
+	}
+	return num;
+}
+
+int sizeOfSet(int i){
+	int siz=0;
+	int par=FindSet(i);
+	loop(j, 0, p.size()){
+		if(par==FindSet(j))
+			++siz;
+	}
+	return siz;
+}
